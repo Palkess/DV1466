d978831bd09e9b020ba79fa1e946c47ae7651edb
Jonas <jonas.andersson.web@gmail.com>
2017-02-21 15:01:30 +0100

    First revision of page-scanner, re-check for script2

---
+++ project/script1.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Page scanner - Scans the given domain for href-tags
+
+DOMAIN=$1
+
+URL=$2
+
+SOURCE=$(curl $DOMAIN$URL) # Get the source from our given parameters
+
+STR=""
+
+for i in $(echo $SOURCE | grep -Po '(?<=href=")[^"]*' ); do # Get all the href-tags value and loop through
+  if [[ $i == *"$DOMAIN"* ]] || [[ $i != "http"* ]]; then # Only save the records that is within the domain
+    TARGETDOMAIN=$DOMAIN
+    if [[ $i == "/"* ]]; then
+      TARGETURL=$i
+    else
+      TARGETURL="/$i"
+    fi
+    STR="$STR$DOMAIN $URL $TARGETDOMAIN $TARGETURL\n"
+  fi
+done
+
+echo -e $STR
