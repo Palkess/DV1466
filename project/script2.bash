0c111480042f39630aa2f92d69d88be17bbc549e
Jonas <jonas.andersson.web@gmail.com>
2017-03-01 21:19:01 +0100

    Changed name on the worklist-file according to descr

--- project/script2.bash
+++ project/script2.bash
@@ -2,17 +2,17 @@
 
 # Dispatch loop - Take the structured output from our Page scanner and loop through them again with Page scanner
 
-$(./script1.bash mechani.se /unix/public/cw4a.html > worklist.txt)
+$(./script1.bash mechani.se /unix/public/cw4a.html > result)
 
-STR=$(cat worklist.txt)
+STR=$(cat result)
 
 VISITED_URLS="" # Holds our visited urls
 OUTPUT=""
 
-while read -r line; do
-  read -a fields <<< "${line}"
+while read -r line; do # Process every line
+  read -a fields <<< "${line}" # Split the line into an array
 
-  if [[ ${fields[3]} == *".html" ]] && [[ $VISITED_URLS != *${fields[3]}* ]]; then # ---------------------- Check dupes in output
-    $(./script1.bash ${fields[2]} ${fields[3]} >> worklist.txt)
+  if [[ ${fields[3]} == *".html" ]] && [[ $VISITED_URLS != *${fields[3]}* ]]; then # Ignore if it's not a html-file or if we already scanned it
+    $(./script1.bash ${fields[2]} ${fields[3]} >> result) # Append the output into our result-file
   fi
 done <<< "$STR"
